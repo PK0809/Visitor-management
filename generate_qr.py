ba48b1b310523bfd0111d8239830eab4ece8b8bc
PK0809 <nagarajnadahrts33@gmail.com>
2025-10-15 13:08:10 +0530

    Update generate_qr.py

--- generate_qr.py
+++ generate_qr.py
@@ -2,7 +2,7 @@
 import os
 
 # URL to open when QR is scanned
-registration_url = "http://127.0.0.1:5000/register"
+registration_url = "https://visitor-management-eqd9.onrender.com/register"
 
 # Folder to save QR code
 qr_folder = os.path.join('static', 'qr_codes')
@@ -16,3 +16,4 @@ img_path = os.path.join(qr_folder, "visitor_registration.png")
 img.save(img_path)
 
 print(f"QR code saved at {img_path}")
+
